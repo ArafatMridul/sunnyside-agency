89840442214a399a710fc5c997a1bbc6a87ef4ec
Arafat Mridul <mridularafat735@gmail.com>
2024-11-22 02:27:31 +0600

    updated

--- app.js
+++ app.js
@@ -1,8 +1,24 @@
 const hamburger = document.querySelector("#hamburger");
 const hamSvg = document.querySelector("#hamburger svg");
 const sideBar = document.querySelector("#sidebar");
+const path = document.querySelector("#path");
 
 hamburger.addEventListener("click", () => {
-    sideBar.classList.toggle("top-[-350px]");
-    sideBar.classList.toggle("top-[110px]");
+    path.classList.toggle("opacity-60");
+    if (sideBar.classList.contains("translate-y-[450px]")) {
+        // sidebar closed
+        document.documentElement.style.overflow = "scroll";
+        document.body.scroll = "yes";
+
+        sideBar.classList.remove("translate-y-[450px]");
+        sideBar.classList.add("translate-y-[0]");
+    } else {
+        // Sidebar opened
+        document.documentElement.style.overflow = "hidden";
+        document.body.scroll = "no";
+        
+        sideBar.classList.remove("translate-y-[0]");
+        sideBar.classList.add("translate-y-[450px]");
+    }
+    
 });
